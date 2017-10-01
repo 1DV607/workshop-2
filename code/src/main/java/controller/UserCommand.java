310e17fbe23a2cb7af6cc99695a281a9291d8ca7
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-10-01 20:38:51 +0200

    add commentary UserInterface and UserCommand

--- code/src/main/java/controller/UserCommand.java
+++ code/src/main/java/controller/UserCommand.java
@@ -6,16 +6,22 @@ package controller;
  */
 public enum UserCommand {
 
-    AddMember,
-    ViewMember,
-    EditMember,
-    RemoveMember,
-    AddBoat,
-    EditBoat,
-    RemoveBoat,
-    ChangeList,
-    Exit;
+    AddMember,              // = 0
+    ViewMember,             // = 1
+    EditMember,             // = 2
+    RemoveMember,           // = 3
+    AddBoat,                // = 4
+    EditBoat,               // = 5
+    RemoveBoat,             // = 6
+    ChangeList,             // = 7
+    Exit;                   // = 8
 
+    /**
+     * Returns the value of an integer -1 (to get the correct value from the array)
+     * @param i - int, Command
+     * @return the value of the command integer - 1
+     * @throws ArrayIndexOutOfBoundsException
+     */
     public static UserCommand getCommand(int i) throws ArrayIndexOutOfBoundsException {
         return UserCommand.values()[i-1];
     }
--- code/src/main/java/view/UserInterface.java
+++ code/src/main/java/view/UserInterface.java
@@ -4,25 +4,27 @@ import javax.json.JsonArray;
 import javax.json.JsonObject;
 
 /**
+ * interface that connects the Controller and the View
  *
+ * Implementing Classes: ConsoleView
  */
 public interface UserInterface {
 
-    public void displayVerboseList(JsonArray jsonArray);
+    public void displayVerboseList(JsonArray jsonArray);                                //called to show a verbose list of members
 
-    public void displayCompactList(JsonArray jsonArray);
+    public void displayCompactList(JsonArray jsonArray);                                //called to show a compact list of members
 
-    public void displayAddMember();
+    public void displayAddMember();                                                     //called to add a member
 
-    public void displayMemberInformation(JsonObject jsonMember, JsonArray jsonBoats);
+    public void displayMemberInformation(JsonObject jsonMember, JsonArray jsonBoats);   //called to view a specific members info
 
-    public void displayEditMember(JsonObject jsonMember);
+    public void displayEditMember(JsonObject jsonMember);                               //called to edit a members information
 
-    public void displayAddBoat(JsonObject jsonMember);
+    public void displayAddBoat(JsonObject jsonMember);                                  //called to add a boat to a member
 
-    public void displayEditBoat(JsonObject jsonMember, JsonObject jsonBoat);
+    public void displayEditBoat(JsonObject jsonMember, JsonObject jsonBoat);            //called to edit a boats information
 
-    public void displayError(String message);
+    public void displayError(String message);                                           //called to display error messages
 
-    public void displayWelcome();
+    public void displayWelcome();                                                       //called to display welcome message
 }
